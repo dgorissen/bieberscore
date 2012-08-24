d51e6a78b48c79a324f722d8d8fab5194960a048
Dirk Gorissen <dgorissen@gmail.com>
2012-08-24 23:14:14 +0100

    got basic top artist/album/track fetching working

--- project.clj
+++ project.clj
@@ -4,4 +4,10 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main bieberscore.core
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [
+                 [org.clojure/clojure "1.4.0"]
+                 [clj-http "0.5.3"]
+                 [org.clojure/data.zip "0.1.1"]
+                 [org.clojars.gjahad/debug-repl "0.3.1"]
+                 ]
+  )
--- src/bieberscore/core.clj
+++ src/bieberscore/core.clj
@@ -1,6 +1,62 @@
-(ns bieberscore.core)
+(ns bieberscore.core
+  (:require [clj-http.client :as client]
+            [clojure.xml :as xml]
+            [clojure.zip :as zip]
+            )
+  (:use clojure.pprint)
+  (:use clojure.data.zip.xml)
+  ;(:use alex-and-georges.debug-repl)
+
+)
+
+(def API-KEY "935ae9bb866dee493ee4bb2e28faecf2")
+(def BASE-URL "http://ws.audioscrobbler.com/2.0/")
+
+(defn string-to-xml [string]
+  (zip/xml-zip
+   (xml/parse
+    (java.io.ByteArrayInputStream.
+      (.getBytes (.trim string))))))
+
+(defn top-artists [user]
+  (let [
+        period "3month"
+        data ((client/get BASE-URL {:query-params {"method" "user.gettopartists" 
+                                                   "user" user 
+                                                   "limit" "50"
+                                                   "api_key" API-KEY 
+                                                   "period" period}}) :body)
+        xmls (string-to-xml data)
+        ]
+    (xml-> xmls :topartists :artist :name text)
+   )
+)
+
+(defn top-albums [artist]
+  (let [
+        data ((client/get BASE-URL {:query-params {"method" "artist.gettopalbums"
+                                                   "artist" artist
+                                                   "limit" "50"
+                                                   "api_key" API-KEY}}) :body)
+        xmls (string-to-xml data)
+        ]
+    (xml-> xmls :topalbums :album :mbid text)
+   )
+)
+
+(defn album-tracks [mbid]
+  (let [
+        data ((client/get BASE-URL {:query-params {"method" "album.getinfo"
+                                                   "mbid" mbid
+                                                   "api_key" API-KEY}}) :body)
+        xmls (string-to-xml data)
+        ]
+    (xml-> xmls :album :tracks :track :name text)
+   )
+)
 
 (defn -main
   "I don't do a whole lot."
   [& args]
-  (println "Hello, World!"))
+  (println (album-tracks (first (top-albums (first (top-artists "dgorissen"))))))
+  )
