341e3f01b674d49d7479771a50aa995a08ff6c7a
Dirk Gorissen <dgorissen@gmail.com>
2012-08-31 21:23:00 +0100

    should roughly work end-to-end now: - getting top artists for lastfm user - getting the top albums for each artist - getting the tracks of each album - getting the lyrics of each track

--- src/bieberscore/core.clj
+++ src/bieberscore/core.clj
@@ -7,8 +7,9 @@
   (:use clojure.pprint)
   (:use clojure.data.zip.xml)
   ;(:use alex-and-georges.debug-repl)
-
 )
+
+(import java.net.URLEncoder)
 
 (def API-KEY "935ae9bb866dee493ee4bb2e28faecf2")
 (def BASE-URL "http://ws.audioscrobbler.com/2.0/")
@@ -19,54 +20,83 @@
     (java.io.ByteArrayInputStream.
       (.getBytes (.trim string))))))
 
+
+(defn get-lastfm-url [url params]
+  (let [response (client/get url (assoc params :throw-exceptions false))]
+    (if (= (response :status) 200)
+      (string-to-xml (response :body))
+      nil)))
+
 (defn top-artists [user]
   (let [
         period "3month"
-        data ((client/get BASE-URL {:query-params {"method" "user.gettopartists" 
-                                                   "user" user 
-                                                   "limit" "50"
-                                                   "api_key" API-KEY 
-                                                   "period" period}}) :body)
-        xmls (string-to-xml data)
+        data (get-lastfm-url BASE-URL {:query-params {"method" "user.gettopartists"
+                                                        "user" user
+                                                        "limit" "5"
+                                                        "api_key" API-KEY
+                                                        "period" period}})
         ]
-    (xml-> xmls :topartists :artist :name text)
-   )
-)
+    (if (nil? data)
+      []
+      (xml-> data :topartists :artist :name text))))
+
 
 (defn top-albums [artist]
   (let [
-        data ((client/get BASE-URL {:query-params {"method" "artist.gettopalbums"
+        data (get-lastfm-url BASE-URL {:query-params {"method" "artist.gettopalbums"
                                                    "artist" artist
-                                                   "limit" "50"
-                                                   "api_key" API-KEY}}) :body)
-        xmls (string-to-xml data)
+                                                   "limit" "3"
+                                                   "api_key" API-KEY}})
         ]
-    (xml-> xmls :topalbums :album :mbid text)
-   )
-)
+    (if (nil? data)
+      []
+      (xml-> data :topalbums :album :mbid text))))
 
 (defn album-tracks [mbid]
   (let [
-        data ((client/get BASE-URL {:query-params {"method" "album.getinfo"
-                                                   "mbid" mbid
-                                                   "api_key" API-KEY}}) :body)
-        xmls (string-to-xml data)
+        data (get-lastfm-url BASE-URL {:query-params {"method" "album.getinfo"
+                                                      "mbid" mbid
+                                                       "api_key" API-KEY}})
         ]
-    (xml-> xmls :album :tracks :track :name text)
-   )
-)
+    (if (nil? data)
+      []
+      (let [artist (first (xml-> data :album :artist text)) ]
+        (map vector (repeat artist) (xml-> data :album :tracks :track :name text))))))
+
+(defn gen-wikia-url [artist title]
+  (str "http://lyrics.wikia.com/"
+    (URLEncoder/encode (clojure.string/replace artist " " "_"))
+     ":"
+    (URLEncoder/encode (clojure.string/replace title " " "_"))))
 
 (defn scrape-lyrics [artist title]
-  (let [url (str "http://lyrics.wikia.com/" artist ":" title)
-        data (html/html-resource (java.net.URL. url))
-        ]
-    (map html/text (html/at (html/select data [:div.lyricbox]) [:div.rtMatcher] nil))
-    )
-)
+  (let [url (gen-wikia-url artist title)]
+
+    (try
+      (let [data (html/html-resource (java.net.URL. url))]
+        ; Im sure this can be done more concisely, get the lyricbox div, remove the rtMatcher div
+        ; and select the text nodes from whats left
+        (filter
+          #(> (count %1) 1) 
+          (html/select (html/at (html/select data [:div.lyricbox]) [:div.rtMatcher] nil) [html/text-node] )))
+      (catch java.io.FileNotFoundException e '()))))
+
+(defn collect-tracks [user]
+   (map album-tracks (flatten (map top-albums (top-artists "dgorissen")))))
+
 
 (defn -main
   "I don't do a whole lot."
   [& args]
-  (println (album-tracks (first (top-albums (first (top-artists "dgorissen"))))))
-  (pprint (count (scrape-lyrics "eminem" "stan")))
-  )
+   (let [lyrics 
+          (for [albums (collect-tracks "dgorissen")
+            tracks albums
+            :let [artist (first tracks)
+                  title (second tracks)]]
+        
+
+            (scrape-lyrics artist title))]
+
+     (pprint (nth lyrics 4))            
+     )
+ ) 
